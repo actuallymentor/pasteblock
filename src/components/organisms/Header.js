4c2285a81088b7ddd0db1da0245eeb9482966a6f
Ruben <ruben@volware.nl>
2022-07-22 11:49:21 +0200

    :art: archive change

--- src/components/organisms/Header.js
+++ src/components/organisms/Header.js
@@ -33,8 +33,8 @@ export default ( { ...props } ) => <Menu>
 	
     <NavbarContainer>
         <NavbarLinkContainer>
-            <NavbarLink className="nav-link" to="/upload">
-                Upload
+            <NavbarLink className="nav-link" to="/archive">
+                Archive
             </NavbarLink>
         </NavbarLinkContainer>
     </NavbarContainer>
